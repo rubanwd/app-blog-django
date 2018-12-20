154098d05976b1ef1543bea38a57f7c7ec245c85
rubanwd <rubanwd@gmail.com>
2018-12-20 17:04:39 +0200

    templates and inheritance

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def posts_list(request):
-    return render(request, 'blog/index.html')
+    n = ['Serhii', 'Kate', 'Andrii', 'Masha']
+    return render(request, 'blog/index.html', context={'names': n})
 
