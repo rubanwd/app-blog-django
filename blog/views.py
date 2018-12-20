d8b56ebfd0729aa92f763de4b31a5e7d1226a7ee
rubanwd <rubanwd@gmail.com>
2018-12-20 19:02:36 +0200

    create model,templates index, Details

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 
 def posts_list(request):
-    n = ['Serhii', 'Kate', 'Andrii', 'Masha']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
 
