c4485ac49692d3eb55a4675cfe4079a31d8e25f3
rubanwd <rubanwd@gmail.com>
2019-01-02 16:52:50 +0200

    Edit Post and Tag

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .utils import *
 from .forms import TagForm, PostForm
 
 # Create your views here.
@@ -15,11 +15,9 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
-
 
 
 class PostCreate(ObjectCreateMixin, View):
@@ -27,15 +25,26 @@ class PostCreate(ObjectCreateMixin, View):
     template = 'blog/post_create_form.html'
 
 
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    form_model = PostForm
+    template = 'blog/post_update_form.html'
+
+
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
 
-
 class TagCreate(ObjectCreateMixin, View):
     form_model = TagForm
     template = 'blog/tag_create.html'
+
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    form_model = TagForm
+    template = 'blog/tag_update_form.html'
 
 
 def tags_list(request):
